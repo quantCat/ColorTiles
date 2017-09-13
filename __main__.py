e2168446d483af026df1e9aa6ac4761ccedff7f8
quantCat <quantCat@github.com>
2017-09-13 18:34:14 +0300

    Shuffling the desk on start

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from tkinter import *
-import random
+from random import *
 
 game = Tk()
 WIDTH_FIELD = 500
@@ -12,6 +12,23 @@ DESK_SIZE = 8
 CELL_SIZE = int(WIDTH_FIELD / DESK_SIZE)
 chosen_tile = 0
 chosen_tile_color = 0
+colors = []
+colors_in_right_order = []
+
+
+def desk_generation():
+    global colors, colors_in_right_order
+    for i in range(DESK_SIZE):
+        for j in range(DESK_SIZE):
+            color = i + j
+            if color >= 10:
+                color = ord('a') + color - 10
+            else:
+                color = ord('0') + color
+            color = "#" + chr(color) * 6
+            colors_in_right_order.append(color);
+    colors = list(colors_in_right_order)
+    shuffle(colors)
 
 
 def tiles_swapping(event):
@@ -34,19 +51,20 @@ field = Canvas (game, width = WIDTH_FIELD, height = HEIGHT_FIELD)
 field.tag_bind("cell", "<Button-1>", tiles_swapping)
 #print(cell_size)
 
+
+desk_generation()
+print (colors)
+print (colors_in_right_order)
 for i in range (DESK_SIZE):
+    #print("---")
     for j in range (DESK_SIZE):
-        color = i+j
-        if color >= 10:
-            color = ord('a') + color - 10
-        else:
-            color = ord('0') + color
-        color = "#" + chr(color) * 6
-        #print(color)
+        color = colors[i*DESK_SIZE + j]
+        #print(i, j, color)
+        right_color = colors_in_right_order[i*DESK_SIZE + j]
         field.create_rectangle(i * CELL_SIZE, \
                                j * CELL_SIZE, \
                                (i+1) * CELL_SIZE, \
-                               (j+1) * CELL_SIZE, fill = color, tags = ("cell", color))
+                               (j+1) * CELL_SIZE, fill = color, tags = ("cell", right_color))
 
 
 newgamebutton.grid(row=0)
