5213bb18cd82348b55c627779ec007ce4efe3e14
quantCat <quantCat@github.com>
2017-09-14 13:18:24 +0300

    Adding color marks + small refactoring

--- __main__.py
+++ __main__.py
@@ -8,8 +8,8 @@ WIDTH_FIELD = 800
 HEIGHT_FIELD = 700
 BUTTON_WIDTH = 10
 BUTTON_HEIGHT = 1
-DESK_WIDTH = 3
-DESK_HEIGHT = 3
+DESK_WIDTH = 5
+DESK_HEIGHT = 5
 # Max size with current algorithm is 8x9
 CELL_WIDTH = int(WIDTH_FIELD / DESK_WIDTH)
 CELL_HEIGHT = int (HEIGHT_FIELD / DESK_HEIGHT)
@@ -22,6 +22,22 @@ colors_in_right_order = []
 
 # TODO:
 # various desks generating
+
+
+def checking_is_game_finished():
+    for i in range (DESK_WIDTH):
+        for j in range (DESK_HEIGHT):
+            index = i*DESK_HEIGHT + j
+            if field.itemcget(field.find_withtag(index+1), "fill") != colors_in_right_order[index]:
+                return False
+    return True
+
+
+def game_finishing():
+    field.delete("color_mark")
+    field.create_text(WIDTH_FIELD / 2, HEIGHT_FIELD / 2, anchor="center", font=("Purisa", 40), \
+                      text="A WINNER IS YOU!", fill=HIGHLIGHT_COLOR)
+    field.config(state="disabled")
 
 
 def desk_generation():
@@ -41,16 +57,40 @@ def desk_generation():
     shuffle(colors)
 
 
-def checking_is_game_finished():
-    for i in range (DESK_WIDTH):
-        for j in range (DESK_HEIGHT):
-            index = i*DESK_HEIGHT + j
-            # print(field.find_withtag(index+1))
-            # print (field.itemcget(field.find_withtag(index+1), "fill"))
-            # print(colors_in_right_order[index])
-            if field.itemcget(field.find_withtag(index+1), "fill") != colors_in_right_order[index]:
-                return False
-    return True
+def color_marks_coords (rectangle_coords):
+    print (rectangle_coords)
+    oval_rad1 = CELL_WIDTH//6
+    oval_rad2 = CELL_HEIGHT//6
+    x_center = (rectangle_coords[0] + rectangle_coords[2]) / 2.0
+    y_center = (rectangle_coords[1] + rectangle_coords[3]) / 2.0  # arithmetic mean
+    circle_coords = [x_center - oval_rad1, y_center - oval_rad2, x_center + oval_rad1, y_center + oval_rad2]
+    return circle_coords
+
+
+def desk_creating():
+    for i in range(DESK_WIDTH):
+        for j in range(DESK_HEIGHT):
+            index = i * DESK_HEIGHT + j
+            color = colors[index]
+            right_color = colors_in_right_order[index]
+            field.create_rectangle(i * CELL_WIDTH,
+                                   j * CELL_HEIGHT,
+                                   (i + 1) * CELL_WIDTH,
+                                   (j + 1) * CELL_HEIGHT, fill=color, tags=("cell", index + 1, right_color))
+            # print (index, color, end=' ')
+            # print()
+    field.create_oval(color_marks_coords(field.coords(1)),
+                      fill=colors_in_right_order[0],
+                      outline=HIGHLIGHT_COLOR, tag="color_mark")  # nw
+    field.create_oval(color_marks_coords(field.coords(DESK_HEIGHT)),
+                      fill=colors_in_right_order[DESK_HEIGHT-1],
+                      outline=HIGHLIGHT_COLOR, tag="color_mark")  # sw
+    field.create_oval(color_marks_coords(field.coords((DESK_HEIGHT * (DESK_WIDTH - 1) + 1))),
+                      fill=colors_in_right_order[(DESK_HEIGHT * (DESK_WIDTH - 1))],
+                      outline=HIGHLIGHT_COLOR, tag="color_mark")  # ne
+    field.create_oval(color_marks_coords(field.coords(DESK_HEIGHT * DESK_WIDTH)),
+                      fill=colors_in_right_order[DESK_HEIGHT * DESK_WIDTH - 1],
+                      outline=HIGHLIGHT_COLOR, tag="color_mark")  # se
 
 
 def tiles_swapping(event):
@@ -71,12 +111,7 @@ def tiles_swapping(event):
         chosen_tile, chosen_tile_color = 0, 0
         game_is_finished = checking_is_game_finished()
         if game_is_finished:
-            # field.destroy()
-            # game_win_text = Label(game, text="A winner is you!")
-            # game_win_text.grid(row=1, columnspan=3)
-            field.create_text(WIDTH_FIELD/2, HEIGHT_FIELD/2, anchor="center", font=("Purisa", 40),\
-                              text="A WINNER IS YOU!", fill=HIGHLIGHT_COLOR)
-            field.config(state="disabled")
+            game_finishing()
 
 
 newgamebutton = Button(game, width = BUTTON_WIDTH, height = BUTTON_HEIGHT, text = "new game")
@@ -88,17 +123,7 @@ field.tag_bind("cell", "<Button-1>", tiles_swapping)
 
 
 desk_generation()
-for i in range (DESK_WIDTH):
-    for j in range (DESK_HEIGHT):
-        index = i*DESK_HEIGHT + j
-        color = colors[index]
-        right_color = colors_in_right_order[index]
-        field.create_rectangle(i * CELL_WIDTH, \
-                               j * CELL_HEIGHT, \
-                               (i+1) * CELL_WIDTH, \
-                               (j+1) * CELL_HEIGHT, fill = color, tags = ("cell", index+1, right_color))
-        # print (index, color, end=' ')
-    # print()
+desk_creating()
 
 
 newgamebutton.grid(row=0)
